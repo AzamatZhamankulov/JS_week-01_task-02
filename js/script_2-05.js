c1ff6636ca1074cfaf6ff3ab643300ab2e075304
Azamat Zhamankulov <azamat.zhamankulov.kk@gmail.com>
2022-01-29 14:23:03 +0600

    added better solution for 2-05

--- js/script_2-05.js
+++ js/script_2-05.js
@@ -6,3 +6,15 @@ let userCard = prompt("Please enter your card number as bellow", 12345678912345)
 let userCardHiden = userCard.replace(userCard.slice(0, -4), "**********");
 
 document.write(`Dear costumer you are using this card: ${userCardHiden} <br>`);
+
+
+// There is more universal code (solution) for this task see bellow
+// let userCardInput = prompt("Please enter your card number", 12345678912345);
+// let cardLength = userCardInput.length;
+// let replacedNumber = cardLength - 4;
+// let putSymbol = "*";
+// let numberOfSymbol = putSymbol.repeat(replacedNumber);
+// let lastFourNumber = userCardInput.slice(replacedNumber);
+
+// document.write(`Your card number is: ${numberOfSymbol}${lastFourNumber}`);
+// console.log(`Your card number is: ${numberOfSymbol}${lastFourNumber}`);
